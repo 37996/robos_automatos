97cfdfa7cbdae4617cb700cb427ae1b08d7c4f6f
Manoel <37996@ufp.edu.pt>
2019-03-17 18:23:30 +0000

    feat: start text robot

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const readline = require('readline-sync')
-
-function start() {
+const robots = {
+    text: require('./robots/text.js')
+}
+async function start() {
     const content = {}
     
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
+
+    await robots.text(content)
 
     function askAndReturnSearchTerm() {
         return readline.question('Digite um termo de pesquisa da Wikipédia:')
     }
 
     function askAndReturnPrefix(){
-        const prefixes = ['Quem é', 'O que é', 'A história do']
+        const prefixes = ['Who is', 'What is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opção:')
         const selectedPrefixText = prefixes[selectedPrefixIndex]
 
